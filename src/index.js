fc23aabea7015fdd175890a9fa1160caa5e8243a
meeba-dev <mikhailbaum@icloud.com>
2022-01-11 05:51:02 +0400

    init: finish reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    var len = n.toString().length;
+    var num = n.toString();
+    var newNum = [];
+    var counter = 0;
+    for (var i = len - 1; i >= 0; i--) {
+        newNum[counter] = num[i];
+        counter++;
+    }
+    return newNum.join("");
 }

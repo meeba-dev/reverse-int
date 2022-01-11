d25aed46a5d1050afb4766f5f163f0f467c4625b
meeba-dev <mikhailbaum@icloud.com>
2022-01-11 05:53:28 +0400

    feat: add abs function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 module.exports = function reverse (n) {
     var len = n.toString().length;
-    var num = n.toString();
+    var num = Math.abs(n).toString();
     var newNum = [];
     var counter = 0;
     for (var i = len - 1; i >= 0; i--) {
